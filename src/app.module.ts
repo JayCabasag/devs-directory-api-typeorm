faa13d8f80e6bf6262eca0988592516f536eb6ff
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-18 19:21:53 +0800

    Added fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,23 +8,23 @@ import { DevelopersModule } from './developers/developers.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'aws.connect.psdb.cloud',
-      port: 3306,
-      username: 'e9d3vpl1fxchh5x0fmmc',
-      password: 'pscale_pw_qmmjtibhcjdvgXp2Ic7RrmrirDELSFaZvIJBebCOp4w',
-      database: 'devs-directory',
-      entities: [Developer],
-      synchronize: true,
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'aws.connect.psdb.cloud',
+    //   port: 3306,
+    //   username: 'e9d3vpl1fxchh5x0fmmc',
+    //   password: 'pscale_pw_qmmjtibhcjdvgXp2Ic7RrmrirDELSFaZvIJBebCOp4w',
+    //   database: 'devs-directory',
+    //   entities: [Developer],
+    //   synchronize: true,
+    //   ssl: {
+    //     rejectUnauthorized: true,
+    //   },
+    // }),
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    DevelopersModule
+    // DevelopersModule
   ],
   controllers: [AppController],
   providers: [AppService],
